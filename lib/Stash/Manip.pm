f49795887e93b1ef649e65c9c3e0bd3188400303
Jesse Luehrs <doy@tozt.net>
2010-05-10 21:31:52 -0500

    initial commit

---
+++ lib/Stash/Manip.pm
@@ -0,0 +1,71 @@
+package Stash::Manip;
+use Moose;
+
+=head1 NAME
+
+Stash::Manip -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-stash-manip at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Stash-Manip>.
+
+=head1 SEE ALSO
+
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Stash::Manip
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Stash-Manip>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Stash-Manip>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Stash-Manip>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Stash-Manip>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
