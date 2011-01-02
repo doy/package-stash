2eb6ad722147f7ecd0234ef5bbd22704c49b2c35
Jesse Luehrs <doy@tozt.net>
2011-01-02 06:15:32 -0600

    silence deprecation warnings for now

--- lib/Package/Stash.pm
+++ lib/Package/Stash.pm
@@ -53,37 +53,37 @@ use Package::DeprecationManager -deprecations => {
 };
 
 sub add_package_symbol {
-    deprecated('add_package_symbol is deprecated, please use add_symbol');
+    #deprecated('add_package_symbol is deprecated, please use add_symbol');
     shift->add_symbol(@_);
 }
 
 sub remove_package_glob {
-    deprecated('remove_package_glob is deprecated, please use remove_glob');
+    #deprecated('remove_package_glob is deprecated, please use remove_glob');
     shift->remove_glob(@_);
 }
 
 sub has_package_symbol {
-    deprecated('has_package_symbol is deprecated, please use has_symbol');
+    #deprecated('has_package_symbol is deprecated, please use has_symbol');
     shift->has_symbol(@_);
 }
 
 sub get_package_symbol {
-    deprecated('get_package_symbol is deprecated, please use get_symbol');
+    #deprecated('get_package_symbol is deprecated, please use get_symbol');
     shift->get_symbol(@_);
 }
 
 sub get_or_add_package_symbol {
-    deprecated('get_or_add_package_symbol is deprecated, please use get_or_add_symbol');
+    #deprecated('get_or_add_package_symbol is deprecated, please use get_or_add_symbol');
     shift->get_or_add_symbol(@_);
 }
 
 sub remove_package_symbol {
-    deprecated('remove_package_symbol is deprecated, please use remove_symbol');
+    #deprecated('remove_package_symbol is deprecated, please use remove_symbol');
     shift->remove_symbol(@_);
 }
 
 sub list_all_package_symbols {
-    deprecated('list_all_package_symbols is deprecated, please use list_all_symbols');
+    #deprecated('list_all_package_symbols is deprecated, please use list_all_symbols');
     shift->list_all_symbols(@_);
 }
 
