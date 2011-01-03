70d7962e28eed7d00bd07c493f8dd5c01a8e3711
Jesse Luehrs <doy@tozt.net>
2011-01-03 10:31:08 -0600

    whoops, the conflicts plugin takes care of this too

--- inc/MMPackageStash.pm
+++ inc/MMPackageStash.pm
@@ -42,44 +42,11 @@ sub can_cc {
 }
 CAN_CC
 
-    # copied out of moose
-    my $check_conflicts = <<'CHECK_CONFLICTS';
-sub check_conflicts {
-    if (eval { require 'lib/Package/Stash/Conflicts.pm'; 1; }) {
-        if (eval { Package::Stash::Conflicts->check_conflicts; 1 }) {
-            return;
-        }
-        else {
-            my $err = $@;
-            $err =~ s/^/    /mg;
-            warn "***\n$err***\n";
-        }
-    }
-    else {
-        print <<'EOF';
-***
-    Your toolchain doesn't support configure_requires, so Dist::CheckConflicts
-    hasn't been installed yet. You should check for conflicting modules
-    manually using the 'package-stash-conflicts' script that is installed with
-    this distribution once the installation finishes.
-***
-EOF
-    }
-
-    # More or less copied from Module::Build
-    return if $ENV{PERL_MM_USE_DEFAULT};
-    return unless -t STDIN && (-t STDOUT || !(-f STDOUT || -c STDOUT));
-
-    sleep 4;
-}
-CHECK_CONFLICTS
-
     my $template = $self->$orig(@_);
 
-    $template =~ s/(use ExtUtils::MakeMaker.*)/$1\n\ncheck_conflicts();/;
     $template =~ s/(WriteMakefile\()/delete \$WriteMakefileArgs{PREREQ_PM}{'Package::Stash::XS'}\n  unless can_cc();\n\n$1/;
 
-    return $template . $can_cc . $check_conflicts;
+    return $template . $can_cc;
 };
 
 __PACKAGE__->meta->make_immutable;
